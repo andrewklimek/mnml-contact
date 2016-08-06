3b1aaa15c44182e82bbe4a28e670d6ebd0531cec
Andrew J Klimek <andrew.klimek@gmail.com>
2016-08-05 23:08:13 -0400

    Check validity workaround for Safari

--- submit.js
+++ submit.js
@@ -1,30 +1,36 @@
 jQuery( document ).ready( function ( $ ) {
+	
 	$( '#contactmonger-form' ).on( 'submit', function(e) {
 		e.preventDefault();
 
-		// var data = {
-		// 	name: this.querySelector('[name=name]').value,
-		// 	email: this.querySelector('[name=email]').value
-		// };
+		if ( ! this.checkValidity() ) {// stupid custom validation for safari
+			e.currentTarget.insertAdjacentHTML('beforeend','<style>#contactmonger-form :invalid{border-color:#f66;}</style>');
+		} else {
+			
+			// var data = {
+			// 	name: this.querySelector('[name=name]').value,
+			// 	email: this.querySelector('[name=email]').value
+			// };
 		
-		var data = $(e.currentTarget).serialize();
+			var data = $(e.currentTarget).serialize();
 		
-		$.ajax({
-			method: 'POST',
-			url: FORM_SUBMIT.root + 'formmonger/v1/submit',
-			data: data,
-			success : function( response ) {
-				// console.log( response );
-				var button = e.currentTarget.querySelector('input[type=submit]');
-				// e.currentTarget.insertAdjacentHTML('beforebegin','<div class="form-success-message">' + FORM_SUBSCRIBE.success + '</div>');
-				e.currentTarget.innerHTML = '<h3 class="form-success-message">' + FORM_SUBMIT.success + '</h3>';
-				e.currentTarget.className += ' form-successfully-submitted';
-				// e.currentTarget.reset();
-			},
-			fail : function( response ) {
-				// console.log( response );
-				e.currentTarget.insertAdjacentHTML('beforebegin','<div class="form-error">' + FORM_SUBMIT.failure + '</div>');
-			}
-		});
+			$.ajax({
+				method: 'POST',
+				url: FORM_SUBMIT.root + 'formmonger/v1/submit',
+				data: data,
+				success : function( response ) {
+					// console.log( response );
+					var button = e.currentTarget.querySelector('input[type=submit]');
+					// e.currentTarget.insertAdjacentHTML('beforebegin','<div class="form-success-message">' + FORM_SUBSCRIBE.success + '</div>');
+					e.currentTarget.innerHTML = '<h3 class="form-success-message">' + FORM_SUBMIT.success + '</h3>';
+					e.currentTarget.className += ' form-successfully-submitted';
+					// e.currentTarget.reset();
+				},
+				fail : function( response ) {
+					// console.log( response );
+					e.currentTarget.insertAdjacentHTML('beforebegin','<div class="form-error">' + FORM_SUBMIT.failure + '</div>');
+				}
+			});
+		}
 	});
 } );
