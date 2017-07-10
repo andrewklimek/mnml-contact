fad6969104527f0f308e3105dcd821112caf4ecd
Andrew Klimek <andrew.klimek@gmail.com>
2017-07-10 14:08:33 -0400

    disabe button instead of hiding form

--- submit.js
+++ submit.js
@@ -2,24 +2,25 @@
 	function submit(e) {
 		e.preventDefault();
 		var form = this;
+		btn = form.querySelector('input[type=submit]');
 		if ( ! form.checkValidity() ) {// stupid custom validation for safari
 			form.insertAdjacentHTML('beforeend','<style>#mnmlcontact :invalid{border-color:#f66;}</style>');
 		} else {
-			form.querySelector('input[type=submit]').style.visibility = "hidden";
+			btn.disabled = true;
 			var xhr = new XMLHttpRequest();
 			xhr.open('POST', '/wp-json/mnmlcontact/v1/submit');
 			// xhr.setRequestHeader( 'X-WP-Nonce', POST_SUBMITTER.nonce );
 			xhr.onload = function() {
-				form.innerHTML = '<p class="mnmlcontact-success">Thanks!</p>';
+				btn.value = "Thanks!";
 				// form.className += ' mnmlcontact-success';
 			};
 			xhr.onerror = function() {
-				console.log(this.responseText);
+				btn.disabled = false;
 			};
 			xhr.send(new FormData(form));
 			// xhr = null;
 		}
 	}
-	document.getElementById('mnmlcontact').addEventListener('submit', submit );
+	document.getElementById('mnmlcontact').addEventListener('submit', submit);
 })();
 
